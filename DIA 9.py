421c82d961d04479e1d0264d8eb2f886cbb29d2e
Mayron <80633811+MayronS@users.noreply.github.com>
2026-03-10 22:52:05 -0300

    Add files via upload

---
+++ DIA 9.py
@@ -0,0 +1,8 @@
+numFunc = int(input())
+horasTra = int(input())
+valorHora = float(input())
+
+salario = horasTra * valorHora
+
+print("NUMBER =", numFunc)
+print("SALARY = U$ {:.2f}".format(salario))
