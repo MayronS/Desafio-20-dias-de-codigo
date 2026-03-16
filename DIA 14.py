330a8af44d2c8d233cc4b4bcdb437f112d63e4bc
Mayron <80633811+MayronS@users.noreply.github.com>
2026-03-15 23:57:14 -0300

    Add files via upload

---
+++ DIA 14.py
@@ -0,0 +1,11 @@
+valor = input().split()
+
+a = int(valor[0])
+b = int(valor[1])
+c = int(valor[2])
+
+maiorAB= (a+b+abs(a-b))/2
+
+maior = int((maiorAB+c+abs(maiorAB-c))/2)
+
+print("{} eh o maior".format(maior))
